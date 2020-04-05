decd2de96aa5852b9af15256517d009b14a985aa
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-04-05 06:31:32 -0400

    fixed a few bugs

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Megalith [![Build Status](https://travis-ci.org/nicholasadamou/Megalith.svg?branch=master)](https://travis-ci.org/nicholasadamou/Megalith)
+Megalith
 ========
 ![license](https://img.shields.io/apm/l/vim-mode.svg)
 [![Say Thanks](https://img.shields.io/badge/say-thanks-ff69b4.svg)](https://saythanks.io/to/NicholasAdamou)
--- src/setup.sh
+++ src/setup.sh
@@ -17,7 +17,7 @@ declare drive=/mnt/data
 setup_megalith() {
 	echo "$(tput setaf 6)This script will configure your Raspberry Pi as a torrentbox.$(tput sgr0)"
 
-    	read -r -p "$(tput bold ; tput setaf 2)Press [Enter] to begin, [Ctrl-C] to abort...$(tput sgr0)"
+    read -r -p "$(tput bold ; tput setaf 2)Press [Enter] to begin, [Ctrl-C] to abort...$(tput sgr0)"
 
 	update
 	upgrade
@@ -25,12 +25,12 @@ setup_megalith() {
 	apt remove -qqy wolfram-engine
 
 	declare -a PKGS=(
-		"transmission-daemon" 
+		"transmission-daemon"
 		"samba"
 		"samba-common-bin"
-		"exfat-fuse" 
-		"ntfs-3g" 
-		"avahi-daemon" 
+		"exfat-fuse"
+		"ntfs-3g"
+		"avahi-daemon"
 		"jq"
 	)
 
@@ -54,14 +54,12 @@ setup_megalith() {
 
 		# format usb drive as 'ext4' if not already 'ext4'
 		if [ "$partion_type" != "ext4" ] ; then
-			if [ "$TRAVIS" != "true" ]; then
-				read -r -p "$(tput setaf 6)Do you want to format $disk as ext4? [y/N] $(tput sgr0)" choice
-				if [ "$choice" == "y" ] || [ "$choice" == "Y" ] ; then
-					echo "$(tput setaf 6)Formatting Mass Storage Device [$(tput setaf 5)$disk$(tput setaf 6)] as ext4...$(tput sgr0)"
-					mkfs.ext4 "$disk"
-				elif [ "$choice" == "n" ] || [ "$choice" == "N" ] ; then
-					exit 0
-				fi
+			read -r -p "$(tput setaf 6)Do you want to format $disk as ext4? [y/N] $(tput sgr0)" choice
+			if [ "$choice" == "y" ] || [ "$choice" == "Y" ] ; then
+				echo "$(tput setaf 6)Formatting Mass Storage Device [$(tput setaf 5)$disk$(tput setaf 6)] as ext4...$(tput sgr0)"
+				mkfs.ext4 "$disk"
+			elif [ "$choice" == "n" ] || [ "$choice" == "N" ] ; then
+				exit 0
 			fi
 		fi
 
@@ -73,9 +71,14 @@ setup_megalith() {
 
 		sudo mount "$disk" "$target"
 
-		if ! [ -d "$target"/{downloads,incomplete} ] ; then
-			mkdir "$target"/{downloads,incomplete}
-		fi
+		declare -a directories=(
+			"downloads"
+			"incomplete"
+		)
+
+		for directory in "${directories[@]}" ; do
+			mkdir -p "$target/$directory"
+		done
 
 		sudo chown "$user":users "$target"/{downloads,incomplete}
 
@@ -176,16 +179,16 @@ EOL
 	sudo update-rc.d transmission-daemon enable
 
 	ip=$(ifconfig wlan0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1)
-	
+
 	echo "$(tput setaf 6)Setup complete!
 
 		$(tput bold)Connect to: $(tput setaf 3)http://$ip:$rpcPort $(tput setaf 6)and login to the Transmission WebUI.$(tput sgr0)
-	  "
+	"
 }
 
 restart() {
     ask_for_confirmation "Do you want to restart?"
-    
+
     if answer_is_yes; then
         sudo shutdown -r now &> /dev/null
     fi
@@ -208,7 +211,7 @@ main() {
     setup_megalith
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    
+
     restart
 }
 
